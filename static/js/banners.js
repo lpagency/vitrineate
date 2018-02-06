4f22d0f364059a32b4814d1a8b73f290cbd0d0f6
anilentp <anilen.tp@gmail.com>
2018-02-06 17:26:36 -0300

    cambios nuevos sacar banners, cambiar grilla prod, quitar ofertaslinkear quienes somos

--- static/js/banners.js
+++ static/js/banners.js
@@ -2,61 +2,7 @@ function banners(tag)
 {
 	switch(tag)
 	{
-		case "cat1_hombre":
-		case "cat2_poleras_h":
-		case "cat2_chaqueta_h":
-		case "cat2_pantalon_h":
-		case "cat2_camisa_h":
-		case "cat2_short_h":
-		case "cat2_corbatas_h":
-		case "cat2_trajes_de_bano_h":
-			$(".listado_productos_hombre").removeClass("hidden");
-		break;
-
-		case "cat1_mujer":
-		case "cat2_ropa":
-		case "cat2_polera_m":
-        case "cat2_chaqueta_m":
-        case "cat2_pantalon_m":
-        case "cat2_falda":
-        case "cat2_short_m":
-        case "cat2_vestido":
-        case "cat2_zapatos_m":
-        case "cat2_ropa_interior_m":
-        case "cat2_pijama_m":
-			$(".listado_productos_mujer").removeClass("hidden");
-		break;
-
-		case "cat1_traje_bano":
-		case "cat2_bano_m" :
-		case "cat2_bano_h" :
-		case "cat2_bano_n" :
-			$(".listado_productos_traje").removeClass("hidden");
-		break;
-
-		case "cat1_accesorios":
-		case "cat2_aros":
-		case "cat2_collares":
-		case "cat2_cinturon":
-		case "cat2_calcetines":
-		case "cat2_carteras":
-		case "cat2_mochilas":
-			$(".listado_productos_accesorios").removeClass("hidden");
-		break;
-
-		case "cat1_marca":
-			$(".listado_productos_marcas").removeClass("hidden");
-		break;
-
-		case "cat1_ninos":
-		case "cat2_ropa_guagua":
-        case "cat2_ninos":
-        case "cat2_ropa_n":
-        case "cat2_accesorios_n":
-			$(".listado_productos_ninos").removeClass("hidden");
-		break;
-
-		case "rosaria_scarini":
+		case "rosaria_scarinci":
 			$(".listado_productos_marca1").removeClass("hidden");
 		break;
 
@@ -103,13 +49,17 @@ function banners(tag)
 		case "stoff":
 			$(".listado_productos_marca12").removeClass("hidden");
 		break;
-
 		case "yambo":
 			$(".listado_productos_marca13").removeClass("hidden");
 		break;
-
-		case "cat1_oferta":
-			$(".listado_productos_oferta").removeClass("hidden");
+		case "mora":
+			$(".listado_productos_marca14").removeClass("hidden");
 		break;
+		case "paula_mar":
+			$(".listado_productos_marca15").removeClass("hidden");
+		break;
+
+
+		
 	}
 }
